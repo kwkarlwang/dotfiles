083bdfc76c9370424d5dbc76a1e5139e434d5dd7
Karl Wang <kawang@ucsd.edu>
2021-07-22 23:23:15 -0700

    Update formatter

--- vim/.config/nvim/lua/keybindings.lua
+++ vim/.config/nvim/lua/keybindings.lua
@@ -1,19 +1,20 @@
 --------Mappings-----------
 map('n', 'j', 'gj', {noremap=true})
 map('n', 'k', 'gk', {noremap=true})
-map('n', '<Esc>', ':noh<CR>', {noremap=true, silent=true})
+map('n', '<Esc>', ":noh<cr>:echo ''<cr>", {noremap=true, silent=true})
 map('v', '<', '<gv', {noremap=true})
 map('v', '>', '>gv', {noremap=true})
 map('i', 'jk', '<Esc>', {noremap=true})
 map('c', '<Down>', 'wildmenumode() ? "<C-n>" : "\\<Down>"', {expr = true, noremap=true})
 map('c', '<Up>', 'wildmenumode() ? "<C-p>" : "\\<Up>"', {expr = true, noremap=true})
-map('n', '<leader>hr', ':PackerSync<CR>', {noremap=true, silent=true})
 map('t', '<Esc>', '<C-\\><C-n>', {noremap=true})
--- source file
+-- HELP
 map('n', '<leader>hs', ':so %<cr>', {noremap=true, silent=true})
-map('n', '``', ":echo ''<cr>", {noremap=true, silent=true})
--- TODO: move to which key later
+map('n', '<leader>hr', ':so %<cr>:PackerSync<cr>', {noremap=true, silent=true})
+-- FILE
 map('n', '<leader>fs', ':w<cr>', {noremap=true, silent=true})
+
+-- WINDOWS
 map('n', '<leader>ws', ':split<cr>', {noremap=true, silent=true})
 map('n', '<leader>wv', ':vsplit<cr>', {noremap=true, silent=true})
 map('n', '<leader>wd', '<C-w>c', {noremap=true, silent=true})
--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -1,109 +1,128 @@
-return require('packer').startup(function(use)
-
+return require("packer").startup(
+  function(use)
     use "wbthomason/packer.nvim"
 
     -- lsp
-    use  { "neovim/nvim-lspconfig",
-    config=function()
+    use {
+      "neovim/nvim-lspconfig",
+      config = function()
         require "plugins/lsp"
-    end
-}
-use  {"hrsh7th/nvim-compe",
-config=function()
-    require "plugins/compe"
-end
-  }
-
-  use  {"kwkarlwang/vim-dracula", as="dracula"}
-
-  -- comment function
-  use  "preservim/nerdcommenter"
-  use {
-      'hoob3rt/lualine.nvim',
-      requires = {'kyazdani42/nvim-web-devicons', opt = true},
-      config=function()
-          require 'plugins/lualine'
       end
-  }
+    }
+    use {
+      "hrsh7th/nvim-compe",
+      config = function()
+        require "plugins/compe"
+      end
+    }
 
-  -- for telescope
-  use  { "nvim-telescope/telescope.nvim",
-  requires={
-      { "nvim-lua/popup.nvim" },
-      { "nvim-lua/plenary.nvim" }    
-  },
-  config=function()
-      require 'plugins/telescope'
-  end
-  }
+    use {"kwkarlwang/vim-dracula", as = "dracula"}
 
+    -- comment function
+    use "preservim/nerdcommenter"
+    use {
+      "hoob3rt/lualine.nvim",
+      requires = {"kyazdani42/nvim-web-devicons", opt = true},
+      config = function()
+        require "plugins/lualine"
+      end
+    }
 
+    -- for telescope
+    use {
+      "nvim-telescope/telescope.nvim",
+      requires = {
+        {"nvim-lua/popup.nvim"},
+        {"nvim-lua/plenary.nvim"}
+      },
+      config = function()
+        require "plugins/telescope"
+      end
+    }
 
-  use "p00f/nvim-ts-rainbow"
-  --------Tree Sitter-----------
-  use  {"nvim-treesitter/nvim-treesitter",
-  run=":TSUpdate",
-  config = function()
-      require'nvim-treesitter.configs'.setup { 
+    use "p00f/nvim-ts-rainbow"
+    --------Tree Sitter-----------
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      config = function()
+        require "nvim-treesitter.configs".setup {
           ensure_installed = "maintained",
           ignore_install = {"haskell"},
-          highlight = { enable = true },
+          highlight = {enable = true},
           rainbow = {
-              enable = false
+            enable = false
           }
-      }
-  end
-  }
+        }
+      end
+    }
 
-  use  "tpope/vim-surround"
+    use "tpope/vim-surround"
 
-  -- pair brackets 
-  use {
+    -- pair brackets
+    use {
       "windwp/nvim-autopairs",
       config = function()
-          require('nvim-autopairs').setup {
-              disable_filetype = {},
-          }
+        require("nvim-autopairs").setup {
+          disable_filetype = {}
+        }
       end
-  }
+    }
 
-  -- fugitive
-  use {  
-      'tpope/vim-fugitive',
+    -- fugitive
+    use {
+      "tpope/vim-fugitive",
       config = function()
-          map('n', '<leader>gg', ':Git<cr>', {noremap=true})
+        map("n", "<leader>gg", ":Git<cr>", {noremap = true})
       end
-  }
+    }
 
-  -- terminal
-  use {"akinsho/nvim-toggleterm.lua",
-  config = function()
-      require 'plugins/term'
+    -- terminal
+    use {
+      "akinsho/nvim-toggleterm.lua",
+      config = function()
+        require "plugins/term"
+      end
+    }
+
+    -- tree structure
+    use {
+      "kyazdani42/nvim-tree.lua",
+      config = function()
+        local tree_cb = require "nvim-tree.config".nvim_tree_callback
+        map("n", "<leader>op", ":NvimTreeToggle<cr>", {noremap = true, silent = true})
+        g.nvim_tree_bindings = {
+          {key = {"l"}, cb = tree_cb("edit")},
+          {key = {"h"}, cb = tree_cb("close_node")},
+          {key = {"L"}, cb = tree_cb("cd")}
+        }
+      end
+    }
+
+    -- color hex code
+    use {
+      "norcalli/nvim-colorizer.lua",
+      config = function()
+        require "colorizer".setup()
+      end
+    }
+
+    -- change root to git repo
+    use {
+      "/airblade/vim-rooter",
+      config = function()
+        --g.rooter_silent_chdir=1
+        g.rooter_manual_only = 1
+      end
+    }
+
+    -- Formatter.nvim
+    use {
+      "mhartington/formatter.nvim",
+      config = function()
+        require "plugins/formatter"
+        map("n", "<leader>cf", ":Format<cr>", {noremap = true, silent = true})
+      end
+    }
   end
-  }
-  
-  -- tree structure
-  use {"kyazdani42/nvim-tree.lua",
-      config = function()
-          local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-          map('n', '<leader>op', ':NvimTreeToggle<cr>', {noremap=true, silent=true})
-          g.nvim_tree_bindings = {
-              { key = {"l"}, cb = tree_cb("edit")},
-              { key = {"h"}, cb = tree_cb("close_node")}
-          }
-      end
-  }
-
-  use {"norcalli/nvim-colorizer.lua",
-    config = function()
-        require'colorizer'.setup()
-    end
-  }
-
-
-
-
-
-
-
-end)
+)
