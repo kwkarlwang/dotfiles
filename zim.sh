ae9ac9e32ab8def7d47a5a8cb7120a9f739f1697
Karl Wang <kawang@ucsd.edu>
2021-07-28 14:10:00 -0700

    commit

--- zim.sh
+++ zim.sh
@@ -1,6 +1,9 @@
+rm -rf ~/.zimrc ~/.zshrc ~/.zim
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 rm -rf ~/.zimrc ~/.zshrc
 stow zim
 source ~/.zshrc
 zimfw install
+zimfw clean
+zimfw compile
 source ~/.zshrc
