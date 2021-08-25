e1d7a5c09a5a7a5bc6cc5b7ceda1d0b4bdc25d1b
Karl Wang <kawang@ucsd.edu>
2021-08-24 21:32:43 -0700

    commit

--- vim/.config/nvim/lua/plugins/cmp.lua
+++ vim/.config/nvim/lua/plugins/cmp.lua
@@ -41,8 +41,8 @@ cmp.setup {
   mapping = {
     ["<Up>"] = cmp.mapping.select_prev_item(),
     ["<Down>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll(-4),
-    ["<C-f>"] = cmp.mapping.scroll(4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm(
