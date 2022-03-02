3461d606b800acd473b695bff401b963c1bdddf8
karl-wang <karl.wang@tusimple.ai>
2022-03-02 11:19:04 -0800

    update

--- vim/.config/nvim/lua/settings.lua
+++ vim/.config/nvim/lua/settings.lua
@@ -81,12 +81,12 @@ o.splitright = true
 
 --------Auto revert-----------
 o.autoread = true
-vim.cmd([[
-     " trigger `autoread` when files changes on disk
-      autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() != 'c' | checktime | endif
-     " notification after file change
-      autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
-]])
+-- vim.cmd([[
+--      " trigger `autoread` when files changes on disk
+--       autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() != 'c' | checktime | endif
+--      " notification after file change
+--       autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
+-- ]])
 
 --------Terminal-----------
 vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")
