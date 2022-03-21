b1155584076057cb3b09e8e189d8b718cff23a6e
karl-wang <karl.wang@tusimple.ai>
2022-03-21 12:09:28 -0700

    Fix

--- vim/.config/nvim/lua/keybindings.lua
+++ vim/.config/nvim/lua/keybindings.lua
@@ -120,5 +120,5 @@ map("n", "<C-;>", "m`A;<esc>``", NS)
 map("i", "<C-;>", "<C-o>m`<C-o>A;<esc>``", NS)
 
 -- begin and end of line in insert mode
-map("i", "<C-a>", "<C-o>I")
-map("i", "<C-e>", "<C-o>A")
+map("i", "<C-a>", "<C-o>I", NS)
+map("i", "<C-e>", "<C-o>A", NS)
