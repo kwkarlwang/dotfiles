e8d2005661f2d27051db3e65210c7d66cc5a53af
karl-wang <karl.wang@tusimple.ai>
2022-04-13 15:01:45 -0700

    Push

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,5 @@
 require("nvim-treesitter.install").compilers = { "gcc" }
 require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
 	ignore_install = {},
 	indent = {
 		enable = false,
