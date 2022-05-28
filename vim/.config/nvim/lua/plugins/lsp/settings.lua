aef63f71d10c8fac66dccb7da4479a6741f46427
Karl Wang <kawang@ucsd.edu>
2022-05-28 00:11:25 -0700

    Commit

--- vim/.config/nvim/lua/plugins/lsp/settings.lua
+++ vim/.config/nvim/lua/plugins/lsp/settings.lua
@@ -3,6 +3,15 @@ fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "�
 fn.sign_define("DiagnosticSignHint", { texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint" })
 fn.sign_define("DiagnosticSignInfo", { texthl = "DiagnosticSignInfo", text = "", numhl = "DiagnosticSignInfo" })
 
+local diagnostics_active = true
+vim.keymap.set("n", "<leader>tD", function()
+	diagnostics_active = not diagnostics_active
+	if diagnostics_active then
+		vim.diagnostic.show()
+	else
+		vim.diagnostic.hide()
+	end
+end)
 -- update in insert mode
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	update_in_insert = true,
