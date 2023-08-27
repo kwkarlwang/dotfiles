7dba2fe2881bc5f399d5b02db919b2a1baba8baf
Karl Wang <kawang@ucsd.edu>
2023-08-27 12:11:39 -0700

    Push

--- vim/.config/nvim/after/ftplugin/scala.lua
+++ vim/.config/nvim/after/ftplugin/scala.lua
@@ -3,6 +3,8 @@ local metals_config = require("metals").bare_config()
 -- Example of settings
 metals_config.settings = {
 	showImplicitArguments = true,
+	enableSemanticHighlighting = true,
+	testUserInterface = "Test Explorer",
 }
 metals_config.init_options.statusBarProvider = "on"
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -31,5 +33,6 @@ metals_config.on_attach = function(client, bufnr)
 	client.server_capabilities.documentFormattingProvider = true
 	require("metals").setup_dap()
 end
-vim.cmd([[hi link @property @function]])
+-- vim.cmd([[hi link @property @function]])
+vim.opt_global.shortmess:remove("F")
 require("metals").initialize_or_attach(metals_config)
