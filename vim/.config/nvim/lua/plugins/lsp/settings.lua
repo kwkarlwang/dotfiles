81bc06ed39068891cedefa41e7f19bcfbd47ddd1
Karl Wang <karl.wang@databricks.com>
2023-11-18 11:06:14 -0800

    Push

--- vim/.config/nvim/lua/plugins/lsp/settings.lua
+++ vim/.config/nvim/lua/plugins/lsp/settings.lua
@@ -1,6 +1,6 @@
-fn.sign_define("DiagnosticSignError", { texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError" })
-fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarn" })
-fn.sign_define("DiagnosticSignHint", { texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint" })
+fn.sign_define("DiagnosticSignError", { texthl = "DiagnosticSignError", text = "󰅚", numhl = "DiagnosticSignError" })
+fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarn" })
+fn.sign_define("DiagnosticSignHint", { texthl = "DiagnosticSignHint", text = "󰌶", numhl = "DiagnosticSignHint" })
 fn.sign_define("DiagnosticSignInfo", { texthl = "DiagnosticSignInfo", text = "", numhl = "DiagnosticSignInfo" })
 
 local diagnostics_active = true
