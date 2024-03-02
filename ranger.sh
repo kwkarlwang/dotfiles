e0c98e7b9d686a8ca39f2bae7a662a4243fec69e
Karl Wang <karl.wang@databricks.com>
2024-03-01 22:13:27 -0800

    Push

--- ranger.sh
+++ ranger.sh
@@ -1,4 +1,5 @@
 stow ranger
 pip3 install ranger-fm
+pip3 install pynvim
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 git clone https://github.com/jchook/ranger-zoxide.git ~/.config/ranger/plugins/zoxide
