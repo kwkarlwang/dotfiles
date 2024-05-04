8bb11a36d302eb9f08cc20bae600623dd5cbbcc8
Karl Wang <karl.wang@databricks.com>
2024-05-04 14:23:55 -0700

    Push

---
+++ vim/.config/nvim/lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,50 @@
+return {
+	"mikesmithgh/kitty-scrollback.nvim",
+	enabled = true,
+	lazy = true,
+	cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+	event = { "User KittyScrollbackLaunch" },
+	-- version = '*', -- latest stable version, may have breaking changes if major version changed
+	-- version = '^4.0.0', -- pin major version, include fixes and features that do not have breaking changes
+	config = function()
+		require("kitty-scrollback").setup({
+			-- global configurations
+			{
+				visual_selection_highlight_mode = "nvim",
+				callbacks = {
+					-- after_setup = function()
+					-- 	local api = require("kitty-scrollback.api")
+					-- 	vim.keymap.set("n", "q", api.close_or_quit_all)
+					-- end,
+					after_launch = function()
+						local api = require("kitty-scrollback.api")
+						vim.keymap.set("n", "q", api.close_or_quit_all, { buffer = 0 })
+						bufmap(0, "n", "<Esc>", ":noh<cr><cmd>lua require('notify').dismiss()<cr>", NS)
+						vim.opt.winbar = ""
+						vim.opt.signcolumn = "no"
+					end,
+				},
+				status_window = {
+					enabled = false,
+				},
+			},
+			ksb_builtin_get_text_all = {
+				kitty_get_text = {
+					extent = "all",
+					ansi = true,
+				},
+			},
+			get_last_non_empty_output = {
+				kitty_get_text = {
+					extent = "last_non_empty_output",
+					ansi = true,
+				},
+				callbacks = {
+					after_ready = function()
+						vim.api.nvim_feedkeys("G{}", "n", true)
+					end,
+				},
+			},
+		})
+	end,
+}
